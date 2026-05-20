300d4577eb644c6022f0788d09d757c3e8481d17
MD.Al-Amin <alamin1developer@gmail.com>
2026-05-20 08:02:36 +0600

    Add Firebase setup and update project config

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAsEPIl1E1D-x0yHMLm5NPNgXXwolp3B5Q',
+  authDomain: 'todo-alamin-dev.firebaseapp.com',
+  projectId: 'todo-alamin-dev',
+  storageBucket: 'todo-alamin-dev.firebasestorage.app',
+  messagingSenderId: '324531740617',
+  appId: '1:324531740617:web:fccbd156ff351c6a089218',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+/* VITE_APP_DATABASE_URL */
